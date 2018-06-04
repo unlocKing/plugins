2c4fd3333f96a23f4bf35802430e26a916770c38
back-to <backto@protonmail.ch>
2018-06-04 17:08:07 +0200

    Moved files

--- tests/test_balticlivecam.py
+++ tests/test_balticlivecam.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.streamlink.plugins.balticlivecam import BalticLivecam
+from ..src.streamlink.plugins.balticlivecam import BalticLivecam
 
 
 class TestPluginBalticLivecam(unittest.TestCase):
